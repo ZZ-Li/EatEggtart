f89fd4c27cf21da4ad38030f663fbdceb75fa859
zz不知道 <zz821199495@163.com>
2018-01-30 19:26:40 +0800

    Initial commit

--- app/src/main/java/com/example/lzz/knowledge/DetailActivity.java
+++ app/src/main/java/com/example/lzz/knowledge/DetailActivity.java
@@ -8,12 +8,23 @@ import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.example.lzz.knowledge.bean.ZhihuDailyStory;
 import com.example.lzz.knowledge.util.API;
+import com.example.lzz.knowledge.util.HttpUtil;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class DetailActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private WebView webView;
+
+    private Gson gson = new Gson();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +36,34 @@ public class DetailActivity extends AppCompatActivity {
         Intent intent = getIntent();
         //String id = intent.getStringExtra("id");
         int id = intent.getIntExtra("id", 0);
-        Log.d("Knowlegdge", "" + id);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl(API.ZHIHU_NEWS_DETAIL + id);
+
+        requestData(getIntent().getIntExtra("id", 0));
+
+    }
+
+    private void requestData(int id){
+        HttpUtil.sendOKHttpRequest(API.ZHIHU_NEWS_DETAIL + id, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseStr = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ZhihuDailyStory story = gson.fromJson(responseStr ,ZhihuDailyStory.class);
+                        webView.getSettings().setJavaScriptEnabled(true);
+                        webView.setWebViewClient(new WebViewClient());
+                        webView.loadUrl(story.getShare_url());
+                    }
+                });
+
+            }
+        });
+
+
     }
 }
