88493db6a45ac5018fd8ef3d816e6e0b8a3b7a5d
zz不知道 <zz821199495@163.com>
2018-02-06 17:31:22 +0800

    Initial commit

--- app/src/main/java/com/example/lzz/knowledge/DetailActivity.java
+++ app/src/main/java/com/example/lzz/knowledge/DetailActivity.java
@@ -1,10 +1,8 @@
 package com.example.lzz.knowledge;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
