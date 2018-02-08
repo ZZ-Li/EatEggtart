6ecf703dbaaccf3799dd62e70a97fa4a89557b5e
zz不知道 <zz821199495@163.com>
2018-02-08 14:20:00 +0800

    Initial commit

--- app/src/main/java/com/example/lzz/knowledge/home/AnotherFragment.java
+++ app/src/main/java/com/example/lzz/knowledge/home/AnotherFragment.java
@@ -3,6 +3,8 @@ package com.example.lzz.knowledge.home;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,9 @@ import com.example.lzz.knowledge.R;
  */
 
 public class AnotherFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private SwipeRefreshLayout refreshLayout;
 
     public static AnotherFragment newInstance(){
         return new AnotherFragment();
